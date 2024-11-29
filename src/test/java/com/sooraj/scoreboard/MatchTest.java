456fb8665716dd311f7d5eaa039c9fd3678c3e52
sooraj <soorajgreshmam@gmail.com>
2024-11-29 20:05:36 +0100

    test: should not register 3 teams for football match

--- src/test/java/com/sooraj/scoreboard/MatchTest.java
+++ src/test/java/com/sooraj/scoreboard/MatchTest.java
@@ -5,16 +5,26 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class MatchTest {
 
     @Test
-    void shouldRegisterFootballTeamsForMatch(){
+    void shouldRegisterTwoFootballTeamsForMatch(){
         Team homeTeam = new FootBallTeam("Mexico");
         Team awayTeam = new FootBallTeam("Canada");
         Match footBallMatch = new FootBallMatch();
-        footBallMatch.register(homeTeam, awayTeam);
+        footBallMatch.register(List.of(homeTeam, awayTeam));
         assertThat(footBallMatch.getTeams(), equalTo(List.of(homeTeam, awayTeam)));
     }
 
+    @Test
+    void shouldNotRegisterThreeFootballTeamsForMatch(){
+        Team homeTeam = new FootBallTeam("Mexico");
+        Team awayTeam = new FootBallTeam("Canada");
+        Team thirdTeam = new FootBallTeam("France");
+        Match footBallMatch = new FootBallMatch();
+        footBallMatch.register(List.of(homeTeam, awayTeam, thirdTeam));
+        assertThrows(IllegalArgumentException.class, () -> footBallMatch.register(List.of(homeTeam, awayTeam, thirdTeam)));
+    }
 }

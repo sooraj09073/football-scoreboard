ec21d042bca8bc2a7afeae9277881e577fda4815
sooraj <soorajgreshmam@gmail.com>
2024-11-30 15:23:20 +0100

    refactor: football match class for isp

--- src/main/java/com/sooraj/scoreboard/football/FootballMatch.java
+++ src/main/java/com/sooraj/scoreboard/football/FootballMatch.java
@@ -2,27 +2,11 @@ package com.sooraj.scoreboard.football;
 
 import com.sooraj.scoreboard.Match;
 import com.sooraj.scoreboard.ScoreBoard;
-import com.sooraj.scoreboard.Team;
-
-import java.util.List;
-import java.util.Optional;
+import com.sooraj.scoreboard.ScoreManager;
+import com.sooraj.scoreboard.TeamRegistration;
 
 public class FootballMatch extends Match {
     public FootballMatch(ScoreBoard scoreBoard) {
-        super(scoreBoard);
-    }
-
-    @Override
-    protected void validateTeams(List<Team> teams) {
-        if (teams == null || teams.size() != 2) {
-            throw new IllegalArgumentException("A football match must have exactly 2 teams.");
-        }
-    }
-
-    @Override
-    public boolean isReadyToStart() {
-        return Optional.ofNullable(getTeams())
-                        .map(teams -> teams.size()==2)
-                        .orElse(false);
+        super(scoreBoard, new FootballTeamValidator(), new TeamRegistration(), new ScoreManager());
     }
 }

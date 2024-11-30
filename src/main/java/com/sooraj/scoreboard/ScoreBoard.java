667e2190520c5f756638efe49262638e28402ec4
sooraj <soorajgreshmam@gmail.com>
2024-11-30 20:06:55 +0100

    refactor : should update scores correctly to scoreboard

--- src/main/java/com/sooraj/scoreboard/ScoreBoard.java
+++ src/main/java/com/sooraj/scoreboard/ScoreBoard.java
@@ -3,12 +3,13 @@ package com.sooraj.scoreboard;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.*;
 
 public abstract class ScoreBoard {
-    List<Match> matchList = new ArrayList<>();
+    private final List<Match> matchList = new ArrayList<>();
 
     public void setScore(Match matchList) {
         this.matchList.add(matchList);
@@ -31,5 +32,19 @@ public abstract class ScoreBoard {
 
     public void removeMatch(Match match) {
         matchList.remove(match);
+    }
+
+    public Match findLiveMatch(String... teams) {
+        Set<String> testTeamSet = Stream.of(teams).collect(toSet());
+        return matchList.stream()
+                .filter(match -> {
+                    Set<String> matchTeamNames = match.getTeams().stream()
+                            .map(Team::getName)
+                            .collect(Collectors.toSet());
+                    // Check if the team names match exactly
+                    return matchTeamNames.equals(testTeamSet);
+                })
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Live match not found with the specified team names"));
     }
 }

afb50caa4a3570ff611cb91fced774093d1c63b8
sooraj <soorajgreshmam@gmail.com>
2024-11-30 15:25:07 +0100

    refactor: Football Team Validator

---
+++ src/main/java/com/sooraj/scoreboard/football/FootballTeamValidator.java
@@ -0,0 +1,15 @@
+package com.sooraj.scoreboard.football;
+
+import com.sooraj.scoreboard.Team;
+import com.sooraj.scoreboard.TeamValidator;
+
+import java.util.List;
+
+public class FootballTeamValidator implements TeamValidator {
+    @Override
+    public void validateTeams(List<Team> teams) {
+        if (teams == null || teams.size() != 2) {
+            throw new IllegalArgumentException("A football match must have exactly 2 teams.");
+        }
+    }
+}

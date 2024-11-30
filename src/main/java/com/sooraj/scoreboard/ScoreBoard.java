ab84a3cfbc174e813ce6ccdfdf0dfa7999285c6b
sooraj <soorajgreshmam@gmail.com>
2024-11-30 22:24:33 +0100

    feat: Score summary

--- src/main/java/com/sooraj/scoreboard/ScoreBoard.java
+++ src/main/java/com/sooraj/scoreboard/ScoreBoard.java
@@ -3,6 +3,7 @@ package com.sooraj.scoreboard;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -49,6 +50,18 @@ public abstract class ScoreBoard {
     }
 
     public String getSummary() {
-        return "";
+        AtomicInteger counter = new AtomicInteger(0);
+        return matchList
+                .stream()
+                .sorted((m1,m2)->{
+                    int sum1 = m1.getHomeScore() + m1.getAwayScore();
+                    int sum2 = m2.getHomeScore() + m2.getAwayScore();
+                    return (sum1==sum2)?
+                            Integer.compare(matchList.lastIndexOf(m2), matchList.lastIndexOf(m1))
+                            :Integer.compare(sum2, sum1);
+                })
+                .map(match-> counter.incrementAndGet() + ". " + match.getTeams().get(0).getName() + " " +match.getHomeScore() + " - "
+                        + match.getTeams().get(1).getName() + " " + match.getAwayScore()
+                ).collect(Collectors.joining("\n", "", "\n"));
     }
 }

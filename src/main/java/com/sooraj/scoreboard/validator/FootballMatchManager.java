04cfbfc9e2ccaee7e7387f9ceb1363da6383d202
sooraj <soorajgreshmam@gmail.com>
2024-12-01 04:17:41 +0100

    refactor: Two Team Match manager

--- src/main/java/com/sooraj/scoreboard/validator/FootballMatchManager.java
+++ src/main/java/com/sooraj/scoreboard/validator/FootballMatchManager.java
@@ -1,20 +1,9 @@
 package com.sooraj.scoreboard.validator;
 
-import com.sooraj.scoreboard.domain.Match;
 import com.sooraj.scoreboard.service.ScoreBoard;
 
-public class FootballMatchManager extends MatchManager {
+public class FootballMatchManager extends TwoTeamMatchManager {
     public FootballMatchManager(ScoreBoard scoreBoard) {
         super(scoreBoard);
-    }
-
-    @Override
-    public boolean canStart(Match match) {
-        if(match.getTeams() != null && match.getTeams().size() != 2){
-            throw new IllegalStateException("Cannot start the match: Teams registration is not complete.");
-        } else if(match.hasStarted()){
-            throw new IllegalStateException("Cannot start the match: Match has already started.");
-        }
-        return true;
     }
 }
